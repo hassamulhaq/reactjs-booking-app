7c3cbcdc76584b1b4e4d18e4754e45c9a7fb54c3
hassamulhaq <developer.hassam@gmail.com>
2025-01-01 06:54:10 +0500

    Fix: update ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,38 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    ignores: ['dist/**/*'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    ignores: ['dist/**/*'],
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    plugins: {
+      // "react-refresh",
+      // "simple-import-sort"
+    },
+    rules: {
+      'react/jsx-uses-react': 'error',
+      'react/react-in-jsx-scope': 'error',
+      'react/jsx-uses-vars': 'error',
+      'react/prop-types': 'error',
+      // 'react-refresh/only-export-components': 'error',
+      'react/no-unescaped-entities': 'error',
+      'no-unused-vars': 'error',
+      'no-undef': 'error',
+    },
+  },
+  {
+    languageOptions: { globals: globals.browser },
+  },
   pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat['jsx-runtime'], // default was `flat.recommended`
 ];
